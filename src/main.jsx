1ad574a3dc4cc212c8336d4b8ef9977733428171
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-04-07 18:10:16 +0200

    added header and link with react route

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,41 @@ import App from "./App";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Layout/main";
+import Home from "./components/Home/Home";
+import Shop from "./components/Shop/Shop";
+import Collection from "./components/Collection/Collection";
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Contact/Contact";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main>Hello</Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "home",
+        element: <Home></Home>,
+      },
+      {
+        path: "shop",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "collection",
+        element: <Collection></Collection>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+    ],
   },
 ]);
 
